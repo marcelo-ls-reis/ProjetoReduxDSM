bcccf7809ced2a62f536e229663e6978eb0dd97a
XandyGomes <x3.computadores@hotmail.com>
2022-04-28 22:21:42 -0300

    aula

---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,21 @@
+import "./Intervalo.css";
+import React from "react";
+
+import Card from "./Card";
+
+export default props => {
+    return (
+        <Card title="Intervalo de Números" red>
+            <div className="Intervalo">
+                <span>
+                    <span>Mínimo:</span>
+                    <input type="number" value={0} />
+                </span>
+                <span>
+                    <span>Máximo:</span>
+                    <input type="number" value={10} />
+                </span>
+            </div>
+        </Card>
+    )
+}
